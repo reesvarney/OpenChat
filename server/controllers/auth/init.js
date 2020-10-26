4207b3b914376676f42e29be245ebc7430dab9b5
Rees Varney <reesvarney02@gmail.com>
2020-10-26 23:34:38 +0000

    Fix default role assignment for authenticated users

--- server/controllers/auth/init.js
+++ server/controllers/auth/init.js
@@ -1,19 +1,21 @@
 function initialize(passport, db, temp_users) {
+  var defaultPermissions = Object.keys(db.models.Role.rawAttributes)
+  .filter((field) => field.startsWith("permission"))
+  .reduce((obj, key) => {
+    obj[key] = db.models.Role.rawAttributes[key]["defaultValue"];
+    return obj;
+  }, {});
+
   passport.serializeUser(function (user, done) {
     return done(null, user.id);
   });
 
   passport.deserializeUser(function (id, done) {
+    // Check if the id starts with `t::` which is used to denote temporary users. Else they are a normal user
     if (id.startsWith("t::")) {
       if (temp_users[id] === undefined) return done(null, false);
-      var all_permissions = db.models.Role.rawAttributes;
       // In future, check for default role
-      temp_users[id]["permissions"] = Object.keys(all_permissions)
-        .filter((field) => field.startsWith("permission"))
-        .reduce((obj, key) => {
-          obj[key] = all_permissions[key]["defaultValue"];
-          return obj;
-        }, {});
+      temp_users[id]["permissions"] = defaultPermissions;
       temp_users[id]["permissions"].permission_send_message = false;
       return done(null, temp_users[id]);
     } else {
@@ -25,6 +27,9 @@ function initialize(passport, db, temp_users) {
       }).then(function (result) {
         var user = result.dataValues;
         var permissions = {};
+        if(user.Roles.length === 0){
+          permissions = defaultPermissions;
+        }
         user.Roles.some((data) => {
           var role = data.dataValues;
           if (role.isAdmin) {
@@ -49,17 +54,6 @@ function initialize(passport, db, temp_users) {
       });
     }
   });
-
-  //TODO - LOAD THESE DYNAMICALLY AS MODULES
-  passport.use(
-    "pubkey",
-    require('./methods/pubkey.js').strategy({db})
-  );
-
-  passport.use(
-    "anon",
-    require('./methods/anon.js').strategy({temp_users})
-  );
 }
 
 module.exports = initialize;
